9edcceb355b2be4a75f625704015b81f7f06e471
Franklin Simpson <simpson.franklin44@gmail.com>
2022-03-23 12:10:17 -0400

    added deal function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
 window.addEventListener('DOMContentLoaded', function() {
   // Execute after page load
 })
+function dealDeck(){
+  let playerCard1 = deck.pop();
+  let playerCard2 = deck.pop();
+  playerHandList.push(playerCard1);
+  playerHandList.push(playerCard2);
+
+  let dealerCard1 = deck.pop();
+  let dealerCard2 = deck.pop();
+  dealerHandList.push(dealerCard1);
+  dealerHandList.push(dealerCard2);
+
+  for(i=0 ; i < playerHandList.length ; i++){
+    let initialPlayerHand = document.createElement('img');
+    initialPlayerHand.src = playerHandList[i].img;
+    playerHand.appendChild(initialPlayerHand);
+  }
